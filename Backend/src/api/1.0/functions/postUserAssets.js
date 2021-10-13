f693e3c3ef5addc28769cc0d9ba32d76c0d01bcb
Stefan Seibel <stefansei04@gmail.com>
2021-10-13 02:28:14 +0200

    fix/back/assets fix wrong variables in postuserassets

--- Backend/src/api/1.0/functions/postUserAssets.js
+++ Backend/src/api/1.0/functions/postUserAssets.js
@@ -2,12 +2,12 @@ const query = require("../../../index").SQL_PROMISE;
 
 const route = async (req, res) => {
     
-    const symbol = req.body.name;
+    const symbol = req.body.symbol;
     const amount = req.body.amount;
     const userId = req.userData.id;
 
     if (symbol && !isNaN(amount)) {
-        await query(res, "INSERT INTO budgetitem (userId, symbol, amount) VALUES (?,?,?,?)",[userId, symbol, amount]);
+        await query(res, "INSERT INTO userasset (userId, symbol, amount) VALUES (?,?,?);",[userId, symbol, amount]);
         return res.sendStatus(201);
     } else {
         return res.sendStatus(400);
